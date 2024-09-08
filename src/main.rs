219a0073b2c1a03b3a04ad459352f1ab0cc3d5aa
Robin Ruud Kristensen <robin1998@hotmail.no>
2024-09-08 13:13:03 +0200

    feat: add convert Fahrenheit to Celsius

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
     println!("Welcome to converter for temperature");
 
     loop {
+        println!("1. convert from fahrenheit to celsius");
         println!("q for quiting");
 
         let mut input = String::new();
@@ -15,7 +15,29 @@ fn main() {
         match input.as_str() {
             "q" => break,
             _ => continue
-        }
+        if input.trim() == "1" {
+            println!("Input the temperature you want to convert:");
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Error reading input");
 
+            let number: i32 = match input.trim().parse() {
+                Ok(number) => number,
+                Err(_) => continue
+            };
+
+            let convert = convert_to_celsius(number);
+
+            println!("Converted {number}F to {convert}C")
+        } else if input.trim() == "q" {
+            break
+        } else {
+            println!("Invalid input");
+        }
     }
+}
+
+fn convert_to_celsius(value: i32) -> i32 {
+    (value - 32) * 5 / 9
 }
