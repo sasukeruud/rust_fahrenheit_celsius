a5264f7742222bcb4caa60412d3aa91c9ab3d5c3
Robin Ruud Kristensen <robin1998@hotmail.no>
2024-09-08 12:35:55 +0200

    feat: add start menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Welcome to converter for temperature");
+
+    loop {
+        println!("q for quiting");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading input");
+
+        match input.as_str() {
+            "q" => break,
+            _ => continue
+        }
+
+    }
 }
