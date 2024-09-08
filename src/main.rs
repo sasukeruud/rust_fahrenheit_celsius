f9e481922728d1d1b73a62a74e880320434c06c7
Robin Ruud Kristensen <robin1998@hotmail.no>
2024-09-08 13:26:15 +0200

    feat: add Celsius to Fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
 
     loop {
         println!("1. convert from fahrenheit to celsius");
+        println!("2. convert from celsius to fahrenheit");
         println!("q for quiting");
 
         let mut input = String::new();
@@ -30,10 +31,30 @@ fn main() {
 
                 println!("Converted {number}F to {convert}C")
             }
+            "2" => {
+                println!("Input the temperature you want to convert:");
+                for _ in 0..input.len() { input.pop(); }
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Error reading input");
+
+                let number: i32 = match input.trim().parse() {
+                    Ok(number) => number,
+                    Err(_) => continue
+                };
+
+                let convert = convert_to_fahrenheit(number);
+
+                println!("Converted {number}C to {convert}F")
+            }
             "q" => break,
             _ => continue
         }
     }
+}
+
+fn convert_to_fahrenheit(value: i32) -> i32 {
+    value * 9/5 + 32
 }
 
 fn convert_to_celsius(value: i32) -> i32 {
